d2cae2d2d8faa6a4b95fd26faacf283e5a109a4a
Filip Spelina <spelifil@fit.cvut.cz>
2023-12-19 12:00:32 +0800

    skip errorAPI

--- encyScrap.py
+++ encyScrap.py
@@ -6,73 +6,81 @@ import time
 
 
 def getResourceEcyclopediaData(id: int) -> dict:
-  url = f"https://www.simcompanies.com/api/v4/en/0/encyclopedia/resources/1/{id}/"
-  # url = "https://jsonplaceholder.typicode.com/todos/1"
-  print(f"Reaching ;{url}; for data.")
-  headers = {'Accept': 'application/json'}
-  response = requests.get(url, headers=headers, timeout=5)
+    url = f"https://www.simcompanies.com/api/v4/en/0/encyclopedia/resources/1/{id}/"
+    # url = "https://jsonplaceholder.typicode.com/todos/1"
+    print(f"Reaching ;{url}; for data.")
+    headers = {"Accept": "application/json"}
+    response = requests.get(url, headers=headers, timeout=5)
 
-  if response.status_code != 200:
-    raise Exception("Error while fetching data from API")
+    if response.status_code != 200:
+        raise Exception("Error while fetching data from API")
 
-  return response.json()
+    return response.json()
 
 
 def readConfig(configFile: str) -> configparser.ConfigParser:
-  config = configparser.ConfigParser()
+    config = configparser.ConfigParser()
 
-  config.read(configFile)
+    config.read(configFile)
 
-  return config
+    return config
 
 
 def scrapSimcoEncyclopedia():
-  config = readConfig("config.ini")
+    config = readConfig("config.ini")
 
-  if config.getboolean("encyclopediaScrap", "run") is False:
-    print("Aborting simcoEnc scraping, config turned off")
-    return
+    if config.getboolean("encyclopediaScrap", "run") is False:
+        print("Aborting simcoEnc scraping, config turned off")
+        return
 
-  id = int(config.get("encyclopediaScrap", "startID"))
+    id = int(config.get("encyclopediaScrap", "startID"))
 
-  data = getResourceEcyclopediaData(id)
+    try:
+        data = getResourceEcyclopediaData(id)
+    except Exception as excpt:
+        print(excpt)
+        print(f"skipping item ID {id}")
+        id += 1
+        config.set("encyclopediaScrap", "startID", str(id))
+        saveConfigFile(config)
+        return
 
-  data.update({"phase": "normal"})
+    data.update({"phase": "normal"})
 
-  now = datetime.now()
-  current_time = now.strftime("%d.%m.%Y_%H:%M")
+    now = datetime.now()
+    current_time = now.strftime("%d.%m.%Y_%H:%M")
 
-  filename = f'export/{id}_{data["name"]}_{current_time}.json'
+    filename = f'export/{id}_{data["name"]}_{current_time}.json'
 
-  with open(filename, "w") as outfile:
-    json.dump(data, outfile)
+    with open(filename, "w") as outfile:
+        json.dump(data, outfile)
 
-  id += 1
-  config.set("encyclopediaScrap", "startID", str(id))
-  saveConfigFile(config)
+    id += 1
+    config.set("encyclopediaScrap", "startID", str(id))
+    saveConfigFile(config)
 
 
 def saveConfigFile(config: configparser.ConfigParser):
-  with open("config.ini", "w") as configfile:
-    config.write(configfile)
+    with open("config.ini", "w") as configfile:
+        config.write(configfile)
 
 
 def regenConfigFile():
-  config = configparser.ConfigParser()
+    config = configparser.ConfigParser()
 
-  config.add_section("encyclopediaScrap")
-  config.set("encyclopediaScrap", "startID", "1")
-  config.set("encyclopediaScrap", "run", "True")
-  config.set("encyclopediaScrap", "phase", "normal")
-  with open("config.ini", "w") as configfile:
-    config.write(configfile)
+    config.add_section("encyclopediaScrap")
+    config.set("encyclopediaScrap", "startID", "1")
+    config.set("encyclopediaScrap", "run", "True")
+    config.set("encyclopediaScrap", "phase", "normal")
+    with open("config.ini", "w") as configfile:
+        config.write(configfile)
 
 
 # MAIN FUNCTION HERE
 
 if __name__ == "__main__":
-  config = readConfig("config.ini")
-  id = int(config.get("encyclopediaScrap", "startID"))
-  while id <= 145:
-    scrapSimcoEncyclopedia()
-    time.sleep(300)
+    config = readConfig("config.ini")
+    id = int(config.get("encyclopediaScrap", "startID"))
+    while id <= 145:
+        scrapSimcoEncyclopedia()
+        time.sleep(300)
